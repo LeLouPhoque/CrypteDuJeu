95685b488bf4468de39b4fb61a0ff3dd39f50829
Arnaud de Jacquelot du Boisrouvray <a.jacquelot@gmail.com>
2025-04-16 14:28:01 +0200

    [FEAT] Mise à jour complète d'un client

--- src/main/java/fr/eni/premierprojet/bll/services/ClientService.java
+++ src/main/java/fr/eni/premierprojet/bll/services/ClientService.java
@@ -1,5 +1,6 @@
 package fr.eni.premierprojet.bll.services;
 
+import fr.eni.premierprojet.bo.Adresse;
 import fr.eni.premierprojet.bo.Client;
 
 import java.util.List;
@@ -12,4 +13,10 @@ public interface ClientService {
     Optional<Client> findByNom(String nom);
 
     List<Client> findAllByNom(String nom);
+
+    Client update(Client client);
+
+    Client update(Long id, String nom, String prenom, String email, String telephone, Adresse adresse);
+
+    Client update(Long id, String nom, String prenom, String email, String telephone, String rue, String ville, String codePostal);
 }
