676dc116c1dd2b5a79a2cc49313f799f0a4ab260
Arnaud de Jacquelot du Boisrouvray <a.jacquelot@gmail.com>
2025-04-16 13:40:53 +0200

    [FEAT] Service gestion client : ajout

---
+++ src/main/java/fr/eni/premierprojet/bll/services/ClientService.java
@@ -0,0 +1,8 @@
+package fr.eni.premierprojet.bll.services;
+
+import fr.eni.premierprojet.bo.Client;
+
+public interface ClientService {
+
+    Client insert(Client client);
+}
---
+++ src/main/java/fr/eni/premierprojet/bll/services/ClientServiceImpl.java
@@ -0,0 +1,18 @@
+package fr.eni.premierprojet.bll.services;
+
+import fr.eni.premierprojet.bo.Client;
+import fr.eni.premierprojet.dal.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public Client insert(Client client) {
+        return clientRepository.save(client);
+    }
+}
